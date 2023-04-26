b1982dba935bd6acf1cdd487af27f5fa64e36677
Firas Alkhatib <firas88alkhatib@gmail.com>
2023-04-26 19:36:46 +0300

    debug

--- src/pages/api/push.ts
+++ src/pages/api/push.ts
@@ -3,20 +3,24 @@ import { client } from '../../storeService'
 import { webPush } from '../../pushService'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log('sending push')
-  await client.connect()
-  const keys = await client.keys('*')
-  console.log('allkeeeeys', keys)
-  for (const key of keys) {
-    const subscription = await client.get(key)
-    console.log('key and value', key, subscription)
-    if (subscription) {
-      webPush.sendNotification(
-        JSON.parse(subscription)!,
-        JSON.stringify({ title: 'Hello Web Push', message: 'Your web push notification is here!' })
-      )
+  try {
+    console.log('sending push')
+    await client.connect()
+    const keys = await client.keys('*')
+    console.log('allkeeeeys', keys)
+    for (const key of keys) {
+      const subscription = await client.get(key)
+      console.log('key and value', key, subscription)
+      if (subscription) {
+        webPush.sendNotification(
+          JSON.parse(subscription)!,
+          JSON.stringify({ title: 'Hello Web Push', message: 'Your web push notification is here!' })
+        )
+      }
     }
+    await client.disconnect()
+  } catch (e) {
+    console.log('errorrrrrrrrr', e)
   }
-  await client.disconnect()
   res.status(200).json({ result: 'done' })
 }
