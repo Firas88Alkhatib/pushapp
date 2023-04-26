070dd6af69a08c5589b97f5949118fe99dc8e798
Firas Alkhatib <firas88alkhatib@gmail.com>
2023-04-26 19:43:45 +0300

    catch errors

--- src/pages/api/push.ts
+++ src/pages/api/push.ts
@@ -5,22 +5,25 @@ import { webPush } from '../../pushService'
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     console.log('sending push')
-    await client.connect()
+    console.log(client.isOpen)
+    if (!client.isOpen) {
+      await client.connect()
+    }
     const keys = await client.keys('*')
     console.log('allkeeeeys', keys)
     for (const key of keys) {
       const subscription = await client.get(key)
       console.log('key and value', key, subscription)
       if (subscription) {
-        webPush.sendNotification(
+        await webPush.sendNotification(
           JSON.parse(subscription)!,
           JSON.stringify({ title: 'Hello Web Push', message: 'Your web push notification is here!' })
         )
       }
     }
-    await client.disconnect()
+    // await client.disconnect()
   } catch (e) {
-    console.log('errorrrrrrrrr', e)
+    console.log('[Push ERORR] : ---------------------------------------- \n]', e)
   }
   res.status(200).json({ result: 'done' })
 }
